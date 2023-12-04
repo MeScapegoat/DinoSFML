eef4e966e962f9888cc1b8af9d83f68a6420d9d8
Scapegoat <callmescapegoat@gmail.com>
2023-12-04 17:28:21 +0500

    Initial commit

---
+++ build/CMakeFiles/DinoSFML.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DinoSFML.dir/main.cpp.o"
+  "CMakeFiles/DinoSFML.dir/main.cpp.o.d"
+  "DinoSFML"
+  "DinoSFML.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DinoSFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/DinoSFML.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DinoSFML.
+# This may be replaced when dependencies are built.
